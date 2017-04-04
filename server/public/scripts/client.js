edce8df89eb6702a8dbf21d44ee243e89c00ec24
Chris Keller <cjameskeller@gmail.com>
2017-04-04 14:57:29 -0500

    pro done but needs style

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,12 @@
 console.log(data);
+
+var myApp = angular.module("myApp", []);
+
+myApp.controller("appendController", ["$scope", function($scope)
+{
+  $scope.appendListing = data;
+  $scope.delete = function(index)
+  {
+    $scope.appendListing.splice(index, 1);
+  };
+}]);
